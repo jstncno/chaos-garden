4f55e5eb107ac6e2a0860dd0191f02b6449ad116
Justin Cano <jstncno@gmail.com>
2024-05-11 23:17:03 -0700

    Start Angular integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import analogjsangular from "@analogjs/astro-angular";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), analogjsangular()]
 });
